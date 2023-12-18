44d58e1701ebaed609bb4ce9f03f8db4b821ab81
rifat-islam2026 <rifat.info001@gmail.com>
2023-12-18 23:47:15 +0600

    fast commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const mobileMenuButton = document.getElementById("mobile-Menu-Button");
+const mobileMenu = document.getElementById("mobile-Menu");
+
+mobileMenuButton.addEventListener("click", () => {mobileMenu.classList.toggle("hidden")})
